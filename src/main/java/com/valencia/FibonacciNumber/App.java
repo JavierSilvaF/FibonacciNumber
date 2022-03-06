a145d26d2f71566d2bae1a6f48908cb6a9ab4424
Javier Silva <JAVSILVAS123@GMAIL.COM>
2022-03-06 18:58:58 -0500

    First Commit
    Sample Code

--- src/main/java/com/valencia/FibonacciNumber/App.java
+++ src/main/java/com/valencia/FibonacciNumber/App.java
@@ -1,13 +1,39 @@
 package com.valencia.FibonacciNumber;
 
 /**
- * Hello world!
- *
+ * Program that calculates the 10th term of the fibonacci sequence.
+ * @author Javier Silva
+ * @author https://github.com/JavierSilva
+ * @version 1.0
+ * @since 1.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+
+public class App {
+
+    /**
+     * Start the execution of the Fibonacci program and outputs the result
+     * @param args argument of String type.
+     */
+
+    public static void main(String[] args) {
+        int n = 10;
+        double result = FibonacciCalculate(n);
+
+        System.out.printf("The 10th term of the Fibonnaci Sequence is %2.2f", result);
+    }//Main
+
+    /** Method to find the nth term of the fibonacci sequence recursively.
+     * @param n Number of times we're repeating the fibonacci sequence.
+     * @return long value of the nth term of the fibonacci sequence
+     */
+    public static long FibonacciCalculate(long n) {
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1) {
+            return n;
+        } else
+            return FibonacciCalculate(n - 1) + FibonacciCalculate(n - 2);
+    }// Class
 }
